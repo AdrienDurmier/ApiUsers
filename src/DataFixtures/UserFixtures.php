4d836cb1638c287f472a339234910554ba2f7d76
AdrienDurmier <a.durmier.dev@gmail.com>
2019-08-28 21:43:43 +0200

    add user select

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -29,9 +29,46 @@ class UserFixtures extends Fixture
         $user->setEnabled(1);
         $user->addRole('ROLE_ADMIN');
         $user->addRole('ROLE_PLANESWALKERS_ADMIN');
+        $user->addRole('ROLE_AGENDA_ADMIN');
         $user->setPassword($this->encoder->encodePassword($user, 'test'));
-
-        $manager->persist($user);
+		$manager->persist($user);
+		
+		$user1 = new User();
+        $user1->setUsername('k.herpelinck@gmail.com');
+        $user1->setFirstname('Kathy');
+        $user1->setLastname('Herpelink');
+        $user1->setEmail('k.herpelinck@gmail.com');
+        $user1->setEnabled(1);
+        $user1->addRole('ROLE_ADMIN');
+        $user1->addRole('ROLE_PLANESWALKERS_ADMIN');
+        $user1->addRole('ROLE_AGENDA_ADMIN');
+        $user1->setPassword($this->encoder->encodePassword($user1, 'test'));
+		$manager->persist($user1);
+		
+		$user2 = new User();
+        $user2->setUsername('achille.durmier@gmail.com');
+        $user2->setFirstname('Achille');
+        $user2->setLastname('Durmier');
+        $user2->setEmail('achille.durmier@gmail.com');
+        $user2->setEnabled(1);
+        $user2->addRole('ROLE_ADMIN');
+        $user2->addRole('ROLE_PLANESWALKERS_ADMIN');
+        $user2->addRole('ROLE_AGENDA_ADMIN');
+        $user2->setPassword($this->encoder->encodePassword($user2, 'test'));
+		$manager->persist($user2);
+		
+		$user3 = new User();
+        $user3->setUsername('clemence.charbonnel@gmail.com');
+        $user3->setFirstname('Clémence');
+        $user3->setLastname('Charbonnel');
+        $user3->setEmail('clemence.charbonnel@gmail.com');
+        $user3->setEnabled(1);
+        $user3->addRole('ROLE_ADMIN');
+        $user3->addRole('ROLE_PLANESWALKERS_ADMIN');
+        $user3->addRole('ROLE_AGENDA_ADMIN');
+        $user3->setPassword($this->encoder->encodePassword($user3, 'test'));
+		$manager->persist($user3);
+        
         $manager->flush();
     }
 }
